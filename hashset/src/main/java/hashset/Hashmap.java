5be7ad98210b27980d1380cf1935c33657badb95
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-02-07 17:36:59 +0530

    Update Hashmap.java

--- hashset/src/main/java/hashset/Hashmap.java
+++ hashset/src/main/java/hashset/Hashmap.java
@@ -1,6 +1,5 @@
 package hashset;
 import java.util.*;
-import java.util.logging.Logger;
 public class Hashmap {
 	
 	public static void main(String[] args)
@@ -10,18 +9,18 @@ public class Hashmap {
 		hs.put(2,"Arun");
 		hs.put(3,"balu");
 		hs.put(4,"krish");
-		System.out.println(hs);
+		logger.log(hs);
 		//to print values alone
 		for(String  i : hs.values())
 		{
-			System.out.println(i);
+			logger.log(i);
 		}
 		hs.replace(2,"krishna");
 		hs.replace(1,"Akshi");
-		System.out.println(hs);
+		logger.log(hs);
 		hs.remove(2);
 		hs.remove(4);
-		System.out.println(hs);
+		logger.log(hs);
 		
 	}
 
