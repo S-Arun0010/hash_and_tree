e23da82ce1861db39fc832e170bd6fdb8001cc86
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-02-07 17:51:10 +0530

    Update Treeset.java

--- hashset/src/main/java/hashset/Treeset.java
+++ hashset/src/main/java/hashset/Treeset.java
@@ -8,7 +8,7 @@ public class Treeset {
 	public static void main(String[] args)
 	{
 		Logger l= Logger.getLogger("com.api.jar");
-		TreeSet tre = new TreeSet();
+		TreeSet<String> tre = new <String>TreeSet();
 	    tre.add("10");
 	    tre.add("Arun");
 	    tre.add("prabu");
@@ -17,8 +17,8 @@ public class Treeset {
 	    tre.add("5");
 	    tre.add("4");
 	    tre.remove("20");
-	    System.out.println(tre);
-	    Iterator i = tre.iterator();
+	    logger.log(tre);
+	    Iterator<String> i = tre.iterator();
         while(i.hasNext())
         {
         	l.info((String) i.next());
